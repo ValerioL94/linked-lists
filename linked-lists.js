f1446b3104c0c5798bba3dce8618f7b37ab1a564
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-02-12 19:21:10 +0100

    Add head and tail functions
    head function returns the first node in
    the linked-list, while tail function returns the
    last one.

--- linked-lists.js
+++ linked-lists.js
@@ -1,20 +1,15 @@
 class LinkedList {
   constructor() {}
   append(value) {
-    if (!this.headNode) {
-      this.headNode = new Node(value);
-    } else {
-      let current = this.headNode;
-      while (current.nextNode) {
-        current = current.nextNode;
-      }
-      current.nextNode = new Node(value);
+    if (!this.headNode) return;
+    let current = this.headNode;
+    while (current.nextNode) {
+      current = current.nextNode;
     }
+    current.nextNode = new Node(value);
   }
   prepend(value) {
-    if (!this.headNode) {
-      return (this.headNode = new Node(value));
-    }
+    this.headNode = new Node(value);
   }
   size() {
     if (!this.headNode) return 0;
@@ -26,7 +21,20 @@ class LinkedList {
     }
     return n;
   }
-  tail() {}
+  head() {
+    if (!this.headNode) return;
+    return this.headNode.value;
+  }
+  tail() {
+    if (!this.headNode) return;
+    let current = this.headNode;
+    let n = 1;
+    while (current.nextNode) {
+      current = current.nextNode;
+      n++;
+    }
+    return current.value;
+  }
   at(index) {}
   pop() {}
   contains(value) {}
@@ -45,9 +53,12 @@ class Node {
 let test = new LinkedList();
 // console.log(test);
 test.prepend(10);
-console.log(test);
+// console.log(test);
 test.append(20);
-console.log(test);
+// console.log(test);
 test.append(30);
-console.dir(test, { depth: null });
-// // console.log(test.headNode);
+// console.dir(test, { depth: null });
+// console.log(test.size());
+// console.log(test.head());
+// console.log(test.tail());
+console.log(test.at(1));
